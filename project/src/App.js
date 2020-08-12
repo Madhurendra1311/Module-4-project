1bb0200d98792dd93ea96c0438fd34b65180f70a
madhurendra1311 <kmadhu1311@gmail.com>
2020-08-12 15:35:05 +0530

    project

--- project/src/App.js
+++ project/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "../src/Component/Home";
+import Welcome from "./Component/Welcome";
 function App() {
   return (
     <div className="App">
       <Home />
+      <Welcome />
     </div>
   );
 }
